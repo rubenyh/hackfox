bf93a4cbe8e71783f615eef4a3d1a6db8c0165d8
PapiHiram <hiramlm@cetys.edu.mx>
2026-05-28 12:26:19 -0700

    feat(navegacion): implementa BottomTabs y cascarones de pantallas

--- mobile-app/src/app/reportar.tsx
+++ mobile-app/src/app/reportar.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function MapScreen() {
+export default function ReportScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Pantalla de Mapa en Vivo (Expo Router)</Text>
+      <Text style={styles.text}>Pantalla de Reportar (Expo Router)</Text>
     </View>
   );
 }
@@ -22,4 +22,3 @@ const styles = StyleSheet.create({
     color: '#333',
   },
 });
-
--- mobile-app/src/components/app-tabs.web.tsx
+++ mobile-app/src/components/app-tabs.web.tsx
@@ -6,7 +6,6 @@ import {
   TabTriggerSlotProps,
   TabListProps,
 } from 'expo-router/ui';
-import { SymbolView } from 'expo-symbols';
 import { Pressable, useColorScheme, View, StyleSheet } from 'react-native';
 
 import { ExternalLink } from './external-link';
@@ -21,11 +20,11 @@ export default function AppTabs() {
       <TabSlot style={{ height: '100%' }} />
       <TabList asChild>
         <CustomTabList>
-          <TabTrigger name="home" href="/" asChild>
-            <TabButton>Home</TabButton>
+          <TabTrigger name="index" href="/" asChild>
+            <TabButton>Mapa</TabButton>
           </TabTrigger>
-          <TabTrigger name="explore" href="/explore" asChild>
-            <TabButton>Explore</TabButton>
+          <TabTrigger name="reportar" href="/reportar" asChild>
+            <TabButton>Reportar</TabButton>
           </TabTrigger>
         </CustomTabList>
       </TabList>
@@ -55,21 +54,10 @@ export function CustomTabList(props: TabListProps) {
     <View {...props} style={styles.tabListContainer}>
       <ThemedView type="backgroundElement" style={styles.innerContainer}>
         <ThemedText type="smallBold" style={styles.brandText}>
-          Expo Starter
+          Tijuana Sin Barreras
         </ThemedText>
 
         {props.children}
-
-        <ExternalLink href="https://docs.expo.dev" asChild>
-          <Pressable style={styles.externalPressable}>
-            <ThemedText type="link">Docs</ThemedText>
-            <SymbolView
-              tintColor={colors.text}
-              name={{ ios: 'arrow.up.right.square', web: 'link' }}
-              size={12}
-            />
-          </Pressable>
-        </ExternalLink>
       </ThemedView>
     </View>
   );
